dd4c4cab6f02339f94fa90c55eddf9ab83947ddc
Bishnu <jasbis2001@gmail.com>
2024-01-10 16:32:42 +0530

    change bg color

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header className="bg-slate-200 shadow-md">
+    <header className="bg-indigo-300 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <Link to="/">
           <h1 className="font-bold text-sm sm:text-x flex flex-wrap">
