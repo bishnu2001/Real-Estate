037bd56698fc3ac79da67d95c56eccc8bbf165e0
Bishnu <jasbis2001@gmail.com>
2024-01-10 16:17:20 +0530

    create user model

---
+++ API/model/user.model.js
@@ -0,0 +1,22 @@
+const mongoose=require("mongoose");
+
+const userSchema=new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    username: {
+        type: String,
+        required: true,
+    }
+},{timestamps:true});
+
+const User=mongoose.model("User",userSchema);
+
+export default User;
