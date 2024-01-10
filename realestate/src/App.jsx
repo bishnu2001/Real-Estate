3a692ccd5479db52e06b7c3c6380ec02b16e58dd
Bishnu <jasbis2001@gmail.com>
2024-01-10 11:27:05 +0530

    create pages and routes

--- realestate/src/App.jsx
+++ realestate/src/App.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
-
+import React from 'react';
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import Home from './pages/Home';
+import Signin from './pages/Signin';
+import Signout from './pages/Signout';
+import About from './pages/About';
+import Profile from './pages/Profile';
 const App = () => {
   return (
-    <div>App</div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<Signin/>} />
+        <Route path="/sign-out" element={<Signout />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
