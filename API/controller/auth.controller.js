aa585360184c7ab60b15fba48e64c3e21ba54149
Bishnu <jasbis2001@gmail.com>
2024-01-12 12:13:28 +0530

    update sigunup api

--- API/controller/auth.controller.js
+++ API/controller/auth.controller.js
@@ -1,7 +1,26 @@
 const { User } = require("../model/user.model.js");
-module.exports.signup=async(req,res)=>{
+const bcrypt = require('bcryptjs');
+
+module.exports.signup = async (req, res) => {
     const { username, email, password } = req.body;
-    const newuser = new User({ username, email, password })
-    await newuser.save();
-    res.status(201).json("user created successfully !")
-}
+
+    // Check if the user already exists
+    const existingUser = await User.findOne({ $or: [{ username }, { email }] });
+    if (existingUser) {
+        return res.status(400).json({ error: "User with the same username or email already exists." });
+    }
+
+    // Hash the password
+    const hashedPassword = bcrypt.hashSync(password, 10);
+
+    // Create a new user
+    const newUser = new User({ username, email, password: hashedPassword });
+
+    try {
+        // Save the new user
+        await newUser.save();
+        res.status(201).json("User created successfully!");
+    } catch (error) {
+        res.status(500).json(error.message);
+    }
+};
--- API/controller/user.controller.js
+++ API/controller/user.controller.js
@@ -1,5 +1,3 @@
-const { default: User } = require("../model/user.model");
-
 module.exports.user=async(req,res)=>{
    
 
