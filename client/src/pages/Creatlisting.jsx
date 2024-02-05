4f71acbcb835d4ab3299ffa8d1370f24c55c1b69
Bishnu <jasbis2001@gmail.com>
2024-02-05 15:09:33 +0530

    complete image upload functionality

--- client/src/pages/Creatlisting.jsx
+++ client/src/pages/Creatlisting.jsx
@@ -1,6 +1,72 @@
-import React from 'react'
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import React from "react";
+import { useState } from "react";
+import { app } from "../firebase";
 
 const Creatlisting = () => {
+  const [files, setFiles] = useState([]);
+  const [formdata, setFormdata] = useState({
+    imageUrl: [],
+  });
+  const [imageuploaderror, setImageuploaderror] = useState(false);
+  const[uploading,setUploading]=useState(false)
+  const handlesubmitimage = (e) => {
+    e.preventDefault();
+    if (files.length > 0 && files.length + formdata.imageUrl.length < 7) {
+        setUploading(true)
+      const promises = [];
+      for (let i = 0; i < files.length; i++) {
+        promises.push(storeimage(files[i]));
+      }
+      Promise.all(promises)
+        .then((url) => {
+          setFormdata({ ...formdata, imageUrl: formdata.imageUrl.concat(url) });
+          setImageuploaderror(false);
+          setUploading(false)
+        })
+        .catch((error) => {
+          setImageuploaderror("Image upload failed (2mb per image)");
+          setUploading(false);
+        });
+    } else {
+      setImageuploaderror("you can upload 6 images per listing");
+    }
+  };
+  const storeimage = async (file) => {
+    return new Promise((resolve, reject) => {
+      const storage = getStorage(app);
+      const filename = new Date().getTime() + file.name;
+      const storageref = ref(storage, filename);
+      const uploadtask = uploadBytesResumable(storageref, file);
+      uploadtask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        },
+
+        (error) => {
+          reject(error);
+        },
+        () => {
+          getDownloadURL(uploadtask.snapshot.ref).then((downloadurl) => {
+            resolve(downloadurl);
+          });
+        }
+      );
+    });
+  };
+  const handleremoveimage = (index) => {
+    setFormdata({
+      ...formdata,
+      imageUrl: formdata.imageUrl.filter((_, i) => i !== index)
+    });
+  };
   return (
     <main className="p-3 max-w-4xl mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">
@@ -120,11 +186,34 @@ const Creatlisting = () => {
               id="images"
               multiple
               className="p-3 border border-gray-300 rounded w-full "
+              onChange={(e) => setFiles(e.target.files)}
             />
-            <button className="p-3 text-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80">
-              upload
+            <button
+              className="p-3 text-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80"
+              onClick={handlesubmitimage}
+            >
+                {uploading?"uploading...":"upload"}
             </button>
           </div>
+          <p className="text-red-500 text-sm">
+            {imageuploaderror && imageuploaderror}
+          </p>
+          {formdata.imageUrl.length > 0 &&
+            formdata.imageUrl.map((url, index) => (
+              <div
+                key={url}
+                className="p-3 text-red-700 rounded-lg uppercase hover:opacity-75 flex justify-between"
+              >
+                <img
+                  src={url}
+                  alt="listing image"
+                  className="w-20 h-20 object-contain rounded-lg"
+                />
+                <button type="button" onClick={()=>handleremoveimage(index)}>
+                  Delete
+                </button>
+              </div>
+            ))}
           <button className="p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
             create listing
           </button>
@@ -132,6 +221,6 @@ const Creatlisting = () => {
       </form>
     </main>
   );
-}
+};
 
 export default Creatlisting;
